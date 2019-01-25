9bc748dee86b879ce12aeadc73bf5dfb3a714051
IlI <mariovaac@unisabana.edu.co>
2019-01-25 08:52:02 -0500

    Pequeño sistema de movimiento
    Space - Salto (Cuando se encuentra en el suelo)
    D - Derecha
    A - Izquierda
    Funciona con .AddForce(), el objeto que se use como player (seleccionado en inspector) acelera cada frame hasta una velocidad maxima.
    Los valores de fuerza de salto, aceleración y velocidad maxima pueden ser cambiados para mejor sensacion de movimiento.
    
    Pd:  Falta hacer que cuando se este presionando una tecla se desactive la friccion del suelo, para no sentir que el objeto solo se mueve rapido mientras se salta.

--- Diluvio Universitario/Assets/Script/MovimientoNaranja.cs
+++ Diluvio Universitario/Assets/Script/MovimientoNaranja.cs
@@ -4,15 +4,55 @@ using UnityEngine;
 
 public class MovimientoNaranja : MonoBehaviour
 {
+    [Header("Set in Inspector")]
+    public GameObject player;
+    public float acceleration;
+    public float jump;
+    public Vector2 actualSpeed;
+    public float maxSpeed;
+    bool onGround;
+    private Rigidbody2D playerRigidbody;
+
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+        playerRigidbody = player.GetComponent<Rigidbody2D>();
 
-    // Update is called once per frame
+    }
+    void OnCollisionStay2D(Collision2D collision)
+    {
+        GameObject coll = collision.gameObject;
+        if (coll.tag == "Ground")
+        {
+            onGround = true;
+        }
+    }
     void Update()
     {
-        
+        playerRigidbody = player.GetComponent<Rigidbody2D>();
+        Vector2 pos = Vector2.zero;
+        if (Input.GetKey(KeyCode.Space) && onGround)
+        {
+            pos.y = jump;
+            playerRigidbody.AddForce(pos);
+            onGround = false;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            if (actualSpeed.x > -maxSpeed)
+            {
+                pos.x = -acceleration;
+                playerRigidbody.AddForce(pos);
+            }
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            if (actualSpeed.x < maxSpeed)
+            {
+                pos.x = acceleration;
+                playerRigidbody.AddForce(pos);
+            }
+        }
+        actualSpeed = playerRigidbody.GetComponent<Rigidbody2D>().velocity;
     }
 }
