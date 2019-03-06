ba10f512014cd27c980ea994c2a161f5f578cff9
IlI <mariovaac@unisabana.edu.co>
2019-03-06 12:15:37 -0500

    JoyStick de movimiento
    JoyStick de Movimiento con vectores.

--- Diluvio Universitario/Assets/Script/Movimiento.cs
+++ Diluvio Universitario/Assets/Script/Movimiento.cs
@@ -1,18 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Movimiento : MonoBehaviour
 {
     [Header("Set in Inspector")]
     GameObject player;
-    public int walkDirection;
+    GameObject touchReference;
     public float xSpeed;
-    public Vector2 dashVector;
+    public float joyX;
+    public float joyY;
+    public float joyMaxScale;
+    public int walkDirection;
     public bool onGround;
-    public bool dash;
     public Vector2 jumpForce;
-    public Vector2 velocityVector;
+    public Text gt;
+    Vector2 left;
+    Vector2 x;
+    float angulo;
+    bool moving;
+
 
     void OnCollisionEnter2D(Collision2D collision)
     {
@@ -20,38 +28,129 @@ public class Movimiento : MonoBehaviour
         if (coll.tag == "Ground")
         {
             onGround = true;
-            dash = true;
+        }
+    }
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        GameObject coll = collision.gameObject;
+        if (coll.tag == "Ground")
+        {
+            onGround = false;
         }
     }
     
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
-    }
+        touchReference= GameObject.FindGameObjectWithTag("touchReference");
+        touchReference.SetActive(false);
+    } 
     void Update()
-    {  
-        if (Input.GetKey(KeyCode.W) && onGround){
+    {
+        if(Input.touchCount>0){
+            for (int i = 0; i < Input.touchCount; i++)
+            {
+                touchReference.transform.position = Camera.main.ScreenToWorldPoint(new Vector3(left.x,left.y, 1f));
+                switch (Input.GetTouch(i).phase)
+                {
+                    case TouchPhase.Began:
+                        touchReference.transform.localScale = Vector2.one;
+                        left = Input.GetTouch(i).position;
+                        touchReference.transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Input.GetTouch(i).position.x, Input.GetTouch(i).position.y, 1f));
+                        touchReference.SetActive(true);
+                        break;
+                    case TouchPhase.Moved:
+                        moving = true;
+                        x = Camera.main.ScreenToWorldPoint(Input.GetTouch(i).position);
+                        x-= new Vector2(touchReference.transform.position.x, touchReference.transform.position.y);
+                        //x.Normalize();
+                        angulo = Mathf.Atan2(x.y, x.x) * Mathf.Rad2Deg;
+                        //gt.text = "angulo: " + angulo.ToString() + "°";
+                        //gt.text = "Vector: " + x.ToString();
+                        if ((Mathf.Abs(x.x) > Mathf.Abs(x.y)) && Mathf.Abs(x.x) < joyMaxScale )
+                        { 
+                        touchReference.transform.localScale = new Vector2(x.x*15, x.x*15);
+                        }else if(((Mathf.Abs(x.x) < Mathf.Abs(x.y)) && Mathf.Abs(x.y) < joyMaxScale))
+                        {
+                        touchReference.transform.localScale = new Vector2(x.y*15, x.y*15);
+                        }
+                        //gt.text = "Escala" + touchReference.transform.localScale.ToString();
+                        break;
+                    case TouchPhase.Ended:
+                        moving = false;
+                        touchReference.SetActive(false);
+                        break;
+                }
+            }
+        }
+
+        //Movimiento con JoyStick : ANGULO
+        //if ((angulo > 45f && angulo < 135f) && onGround && moving)
+        //{
+        //    player.GetComponent<Rigidbody2D>().AddForce(jumpForce);
+        //    onGround = false;
+        //}
+        //else if ((angulo > 135f || angulo < -135f) && moving)
+        //{
+        //    player.GetComponent<Rigidbody2D>().velocity = new Vector2(-xSpeed, player.GetComponent<Rigidbody2D>().velocity.y);
+        //    walkDirection = -1;
+        //}
+        //else if ((angulo < 45f || angulo < -45f) && moving)
+        //{
+        //    player.GetComponent<Rigidbody2D>().velocity = new Vector2(xSpeed, player.GetComponent<Rigidbody2D>().velocity.y);
+        //    walkDirection = 1;
+        //}
+        //if (!moving)
+        //{
+        //    player.GetComponent<Rigidbody2D>().velocity = new Vector2(0, player.GetComponent<Rigidbody2D>().velocity.y);
+        //}
+
+        //Movimiento con JoyStick : MAGNITUD !NORMALIZADO
+        //if ((x.y > 0.4f) && onGround && moving)
+        //{
+        //    player.GetComponent<Rigidbody2D>().AddForce(jumpForce);
+        //    onGround = false;
+        //}
+        //else if ((x.x < -0.4f) && moving)
+        //{
+        //    player.GetComponent<Rigidbody2D>().velocity = new Vector2(-xSpeed, player.GetComponent<Rigidbody2D>().velocity.y);
+        //    walkDirection = -1;
+        //}
+        //else if ((x.x > 0.4f) && moving)
+        //{
+        //    player.GetComponent<Rigidbody2D>().velocity = new Vector2(xSpeed, player.GetComponent<Rigidbody2D>().velocity.y);
+        //    walkDirection = 1;
+        //}
+        //if (!moving)
+        //{
+        //    player.GetComponent<Rigidbody2D>().velocity = new Vector2(0, player.GetComponent<Rigidbody2D>().velocity.y);
+        //}
+
+        //Movimiento con JoyStick : MAGNITUD !NO NORMALIZADO
+        if ((x.y > joyY) && onGround && moving && (player.GetComponent<Rigidbody2D>().velocity.y==0))
+        {
             player.GetComponent<Rigidbody2D>().AddForce(jumpForce);
             onGround = false;
         }
-        if(Input.GetKey(KeyCode.A) && dash)
+        else if ((x.x < -joyX) && moving)
         {
             player.GetComponent<Rigidbody2D>().velocity = new Vector2(-xSpeed, player.GetComponent<Rigidbody2D>().velocity.y);
             walkDirection = -1;
         }
-        if(Input.GetKey(KeyCode.D) )
+        else if ((x.x > joyX) && moving)
         {
             player.GetComponent<Rigidbody2D>().velocity = new Vector2(xSpeed, player.GetComponent<Rigidbody2D>().velocity.y);
             walkDirection = 1;
         }
-        if(Input.GetKeyUp(KeyCode.D) || Input.GetKeyUp(KeyCode.A)){
-        player.GetComponent<Rigidbody2D>().velocity = new Vector2(0, player.GetComponent<Rigidbody2D>().velocity.y);
+        if (!moving)
+        {
+            player.GetComponent<Rigidbody2D>().velocity = new Vector2(0, player.GetComponent<Rigidbody2D>().velocity.y);
         }
-        if (Input.GetKeyDown(KeyCode.E) && dash && (player.GetComponent<Rigidbody2D>().velocity.x == 0)){
-            dashVector.x *= walkDirection;
-            player.GetComponent<Rigidbody2D>().AddForce(dashVector);
-            dash = false;
+
+        //Reset
+        if (player.transform.position.y<-10)
+        {
+            player.transform.position = new Vector2(0,1);
         }
-        velocityVector = player.GetComponent<Rigidbody2D>().velocity;
     }
 }
